1a981dd32ff697663b45a26eebf9615c4592a9ea
athul <athul8720@gmail.com>
2020-01-07 11:50:31 +0530

    Even Pretty

--- tg_gh_action_noti.sh
+++ tg_gh_action_noti.sh
@@ -17,9 +17,9 @@ send_msg() {
 # Send message to the bot with some pertinent details about the job
 # Note that for Markdown, you need to escape any backtick (inline-code)
 # characters, since they're reserved in bash
-if [ ${GITHUB_EVENT_NAME}=="push" ]; then
-    send_msg "
-⬆️⬆️⬆️⬆️
+
+send_msg "
+⬆️📝✨⭐️🗣
 ID: ${GITHUB_WORKFLOW}
 
 Action was a *${status}!*
@@ -32,51 +32,15 @@ By:            *${GITHUB_ACTOR}*
 
 Tag:        ${GITHUB_REF}
 
-[Link to Repo ]("https://github.com/${GITHUB_REPOSITORY}/")
-
-[Build log here]("https://github.com/${GITHUB_REPOSITORY}/commit/${GITHUB_SHA}/checks")
-    -----
-    "
-elif [${GITHUB_EVENT_NAME}=="issues"] || [${GITHUB_EVENT_NAME}=="issue_comment"]; then
-    send_msg "
-‼️🗣‼️🗣
-
-ID: ${GITHUB_WORKFLOW}
-
-Action was a *${status}!*
-
-\`Repository:  ${GITHUB_REPOSITORY}\` 
-
-On:          *${GITHUB_EVENT_NAME}*
-
-By:            *${GITHUB_ACTOR}* 
+------
 
 Issue Title and Number  : ${IU_TITLE} ${IU_NUM}
 
-Commented/ Created By : ${IU_ACTOR}
+Commented or Created By : ${IU_ACTOR}
 
-Issue_Body : ${IU_BODY}
+Issue Body : ${IU_BODY}
 
-[Link to Repo ]("https://github.com/${GITHUB_REPOSITORY}/")
-
-[Link to Profile]("https://github.com/${GITHUB_ACTOR}/")
-
-[Build log here]("https://github.com/${GITHUB_REPOSITORY}/commit/${GITHUB_SHA}/checks")
------
-    "
-elif [${GITHUB_EVENT_NAME}=="pull_request"]; then
-send_msg "
-🔃⤴️🔃⤴️
-
-ID: ${GITHUB_WORKFLOW}
-
-Action was a *${status}!*
-
-\`Repository:  ${GITHUB_REPOSITORY}\` 
-
-On:          *${GITHUB_EVENT_NAME}*
-
-By:            *${GITHUB_ACTOR}*
+------
 
 PR ${PR_STATE} 
 
@@ -86,56 +50,15 @@ PR Title:       ${PR_TITLE}
 
 PR Body:        ${PR_BODY}
 
-[Link to Repo ]("https://github.com/${GITHUB_REPOSITORY}/")
-
-[Link to Profile]("https://github.com/${GITHUB_ACTOR}/")
-
-[Build log here]("https://github.com/${GITHUB_REPOSITORY}/commit/${GITHUB_SHA}/checks")
------
-"
-elif [${GITHUB_EVENT_NAME}=="watch"]; then
-send_msg "
-⭐️✨⭐️✨
-
-ID: ${GITHUB_WORKFLOW}
-
-Action was a *${status}!*
-
-\`Repository:  ${GITHUB_REPOSITORY}\` 
-
-On:          *${GITHUB_EVENT_NAME}*
-
-By:            *${GITHUB_ACTOR}* 
+------
 
 Star Count      ${STARGAZERS}
 
 Fork Count      ${FORKERS}
 
-[Link to Profile]("https://github.com/${GITHUB_ACTOR}/")
+[Link to Repo ]("https://github.com/${GITHUB_REPOSITORY}/")
 
 [Build log here]("https://github.com/${GITHUB_REPOSITORY}/commit/${GITHUB_SHA}/checks")
 
------
-"
-else
-send_msg "
-    ✅⭐️❗️🔀
+    "
 
-    ID: ${GITHUB_WORKFLOW}
-
-    Action was a *${status}!*
-
-    \`Repository:  ${GITHUB_REPOSITORY}\` 
-
-    On:          *${GITHUB_EVENT_NAME}*
-
-    By:            *${GITHUB_ACTOR}* 
-
-    [Link to Repo ]("https://github.com/${GITHUB_REPOSITORY}/")
-
-    [Link to Profile]("https://github.com/${GITHUB_ACTOR}/")
-
-    [Build log here]("https://github.com/${GITHUB_REPOSITORY}/commit/${GITHUB_SHA}/checks")
-    -----
-"
-fi
