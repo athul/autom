434b8624b82e0c16984ee3c70bb9bf4a0be14ab9
athul <athul8720@gmail.com>
2020-01-07 11:34:39 +0530

    Prettier

--- tg_gh_action_noti.sh
+++ tg_gh_action_noti.sh
@@ -19,103 +19,104 @@ send_msg() {
 # characters, since they're reserved in bash
 if [ ${GITHUB_EVENT_NAME}=="push" ]; then
     send_msg "
-    ⬆️⬆️⬆️⬆️
+⬆️⬆️⬆️⬆️
+ID: ${GITHUB_WORKFLOW}
 
-    ID: ${GITHUB_WORKFLOW}
-    Action was a *${status}!*
+Action was a *${status}!*
 
-    \`Repository:  ${GITHUB_REPOSITORY}\` 
+\`Repository:  ${GITHUB_REPOSITORY}\` 
 
-    On:          *${GITHUB_EVENT_NAME}*
+On:          *${GITHUB_EVENT_NAME}*
 
-    By:            *${GITHUB_ACTOR}* 
+By:            *${GITHUB_ACTOR}* 
 
-    Tag:        ${GITHUB_REF}
+Tag:        ${GITHUB_REF}
 
-    [Link to Repo ]("https://github.com/${GITHUB_REPOSITORY}/")
+[Link to Repo ]("https://github.com/${GITHUB_REPOSITORY}/")
 
-    [Build log here]("https://github.com/${GITHUB_REPOSITORY}/commit/${GITHUB_SHA}/checks")
+[Build log here]("https://github.com/${GITHUB_REPOSITORY}/commit/${GITHUB_SHA}/checks")
     -----
     "
 elif [${GITHUB_EVENT_NAME}=="issues"] || [${GITHUB_EVENT_NAME}=="issue_comment"]; then
     send_msg "
-    ‼️🗣‼️🗣
+‼️🗣‼️🗣
 
-    ID: ${GITHUB_WORKFLOW}
+ID: ${GITHUB_WORKFLOW}
 
-    Action was a *${status}!*
+Action was a *${status}!*
 
-    \`Repository:  ${GITHUB_REPOSITORY}\` 
+\`Repository:  ${GITHUB_REPOSITORY}\` 
 
-    On:          *${GITHUB_EVENT_NAME}*
+On:          *${GITHUB_EVENT_NAME}*
 
-    By:            *${GITHUB_ACTOR}* 
+By:            *${GITHUB_ACTOR}* 
 
-    Issue Title and Number  : ${IU_TITLE} ${IU_NUM}
+Issue Title and Number  : ${IU_TITLE} ${IU_NUM}
 
-    Commented/ Created By : ${IU_ACTOR}
+Commented/ Created By : ${IU_ACTOR}
 
-    Issue_Body : ${IU_BODY}
+Issue_Body : ${IU_BODY}
 
-    [Link to Repo ]("https://github.com/${GITHUB_REPOSITORY}/")
+[Link to Repo ]("https://github.com/${GITHUB_REPOSITORY}/")
 
-    [Link to Profile]("https://github.com/${GITHUB_ACTOR}/")
+[Link to Profile]("https://github.com/${GITHUB_ACTOR}/")
 
-    [Build log here]("https://github.com/${GITHUB_REPOSITORY}/commit/${GITHUB_SHA}/checks")
-    -----
+[Build log here]("https://github.com/${GITHUB_REPOSITORY}/commit/${GITHUB_SHA}/checks")
+-----
     "
 elif [${GITHUB_EVENT_NAME}=="pull_request"]; then
-    send_msg "
-    🔃⤴️🔃⤴️
+send_msg "
+🔃⤴️🔃⤴️
 
-    ID: ${GITHUB_WORKFLOW}
+ID: ${GITHUB_WORKFLOW}
 
-    Action was a *${status}!*
+Action was a *${status}!*
 
-    \`Repository:  ${GITHUB_REPOSITORY}\` 
+\`Repository:  ${GITHUB_REPOSITORY}\` 
 
-    On:          *${GITHUB_EVENT_NAME}*
+On:          *${GITHUB_EVENT_NAME}*
 
-    By:            *${GITHUB_ACTOR}*
+By:            *${GITHUB_ACTOR}*
 
-    PR ${PR_STATE} 
+PR ${PR_STATE} 
 
-    PR Number:      ${PR_NUM}
+PR Number:      ${PR_NUM}
 
-    PR Title:       ${PR_TITLE}
+PR Title:       ${PR_TITLE}
 
-    PR Body:        ${PR_BODY}
+PR Body:        ${PR_BODY}
 
-    [Link to Repo ]("https://github.com/${GITHUB_REPOSITORY}/")
+[Link to Repo ]("https://github.com/${GITHUB_REPOSITORY}/")
 
-    [Link to Profile]("https://github.com/${GITHUB_ACTOR}/")
+[Link to Profile]("https://github.com/${GITHUB_ACTOR}/")
 
-    [Build log here]("https://github.com/${GITHUB_REPOSITORY}/commit/${GITHUB_SHA}/checks")
-    -----
-    "
+[Build log here]("https://github.com/${GITHUB_REPOSITORY}/commit/${GITHUB_SHA}/checks")
+-----
+"
 elif [${GITHUB_EVENT_NAME}=="watch"]; then
-    send_msg "
-    ⭐️✨⭐️✨
+send_msg "
+⭐️✨⭐️✨
 
-    ID: ${GITHUB_WORKFLOW}
+ID: ${GITHUB_WORKFLOW}
 
-    Action was a *${status}!*
+Action was a *${status}!*
 
-    \`Repository:  ${GITHUB_REPOSITORY}\` 
+\`Repository:  ${GITHUB_REPOSITORY}\` 
 
-    On:          *${GITHUB_EVENT_NAME}*
+On:          *${GITHUB_EVENT_NAME}*
 
-    By:            *${GITHUB_ACTOR}* 
+By:            *${GITHUB_ACTOR}* 
 
-    Star Count      ${STARGAZERS}
+Star Count      ${STARGAZERS}
 
-    Fork Count      ${FORKERS}
+Fork Count      ${FORKERS}
 
-    [Link to Profile]("https://github.com/${GITHUB_ACTOR}/")
+[Link to Profile]("https://github.com/${GITHUB_ACTOR}/")
 
-    [Build log here]("https://github.com/${GITHUB_REPOSITORY}/commit/${GITHUB_SHA}/checks")
-    -----
-    "
+[Build log here]("https://github.com/${GITHUB_REPOSITORY}/commit/${GITHUB_SHA}/checks")
+
+-----
+"
 else
 send_msg "
     ✅⭐️❗️🔀
