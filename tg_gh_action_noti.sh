df3916c64b8f9a48e8ee4efc7502a7cb1d967256
Athul Cyriac Ajay <athul8720@gmail.com>
2020-01-07 11:42:38 +0530

    Upgrades (#2)
    * Upgrades
    
    * Update Notifier.yml
    
    * Checking Validation
    
    * Prettier
    
    * Prettify

--- tg_gh_action_noti.sh
+++ tg_gh_action_noti.sh
@@ -6,11 +6,10 @@ BOT_URL="https://api.telegram.org/bot${TELEGRAM_TOKEN}/sendMessage"
 # Set formatting for the message. Can be either "Markdown" or "HTML"
 PARSE_MODE="Markdown"
 
-
 status=$INPUT_STATUS
 # Define send message function. parse_mode can be changed to
 # HTML, depending on how you want to format your message:
-send_msg () {
+send_msg() {
     curl -s -X POST ${BOT_URL} -d chat_id=$TELEGRAM_CHAT_ID \
         -d text="$1" -d parse_mode=${PARSE_MODE}
 }
@@ -18,10 +17,11 @@ send_msg () {
 # Send message to the bot with some pertinent details about the job
 # Note that for Markdown, you need to escape any backtick (inline-code)
 # characters, since they're reserved in bash
-send_msg "
-✅⭐️❗️🔀
 
+send_msg "
+⬆️📝✨⭐️🗣
 ID: ${GITHUB_WORKFLOW}
+
 Action was a *${status}!*
 
 \`Repository:  ${GITHUB_REPOSITORY}\` 
@@ -30,12 +30,34 @@ On:          *${GITHUB_EVENT_NAME}*
 
 By:            *${GITHUB_ACTOR}* 
 
-No of Stars:    ${STARGAZERS}
+Tag:        ${GITHUB_REF}
+
+------
+
+Issue Title and Number  : ${IU_TITLE} ${IU_NUM}
+
+Commented or Created By : ${IU_ACTOR}
+
+Issue Body : ${IU_BODY}
+
+------
+
+PR ${PR_STATE} 
+
+PR Number:      ${PR_NUM}
+
+PR Title:       ${PR_TITLE}
+
+PR Body:        ${PR_BODY}
+
+------
+
+Star Count      ${STARGAZERS}
+
+Fork Count      ${FORKERS}
 
 [Link to Repo ]("https://github.com/${GITHUB_REPOSITORY}/")
 
-[Link to Profile]("https://github.com/${GITHUB_ACTOR}/")
-
 [Build log here]("https://github.com/${GITHUB_REPOSITORY}/commit/${GITHUB_SHA}/checks")
------
-"
+
+    "
